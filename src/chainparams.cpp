84577a99db663b95f5cffbe7e87c561409cd04d3
Zarathustra <amerocrypto@gmail.com>
2018-01-21 21:06:36 -0500

    update params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,10 +122,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_DIP0001].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00"); // TODO
+        consensus.nMinimumChainWork = uint256S("0x00"); // TODO: update
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x00"); // TODO
+        consensus.defaultAssumeValid = uint256S("0x0000071a1c6ad87afb3dd6f6f57ea233a22296e638a70dff45d1bde02c445516"); // TODO: update
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -270,10 +270,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_DIP0001].nThreshold = 50; // 50% of 100
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000924e924a21715"); // 37900
+        consensus.nMinimumChainWork = uint256S("0x00"); // TODO: update
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x0000000004f5aef732d572ff514af99a995702c92e4452c7af10858231668b1f"); // 37900
+        consensus.defaultAssumeValid = uint256S("0x00000acfad42298bae13f073402890c5fcd650158f3318326b7e2b1e1a254ae3");
 
         pchMessageStart[0] = 0xc8;
         pchMessageStart[1] = 0xcb;
